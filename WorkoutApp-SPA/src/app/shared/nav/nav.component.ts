0cca5d122faba909fd997311b37b4ae4be21906d
Robert Herber <rwherber@gmail.com>
2020-02-29 15:36:53 -0800

    worked on scss

--- WorkoutApp-SPA/src/app/shared/nav/nav.component.ts
+++ WorkoutApp-SPA/src/app/shared/nav/nav.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from 'src/app/auth/auth.service';
 @Component({
     selector: 'app-nav',
     templateUrl: './nav.component.html',
-    styleUrls: ['./nav.component.css']
+    styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
 
