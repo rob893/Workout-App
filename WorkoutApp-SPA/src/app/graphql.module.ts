63159850e677f52d42560034c465602288b7b207
Robert Herber <rwherber@gmail.com>
2020-03-10 07:02:26 -0700

    worked on refresh tokens.

--- WorkoutApp-SPA/src/app/graphql.module.ts
+++ WorkoutApp-SPA/src/app/graphql.module.ts
@@ -4,6 +4,7 @@ import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { onError } from 'apollo-link-error';
 import { ServerError, ServerParseError } from 'apollo-link-http-common';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
 
 const uri = 'http://localhost:4000';
 
@@ -13,16 +14,44 @@ function isServerError(error: Error | ServerError | ServerParseError): error is 
 
 export function createApollo(httpLink: HttpLink) {
     const http = httpLink.create({ uri });
-    const logoutLink = onError(({ networkError, operation }) => {
-        console.log(operation.getContext());
-        console.log(networkError);
-        if (networkError && isServerError(networkError) && networkError.statusCode === 401) {
-            // authService.logout();
+
+    const afterwareLink = new ApolloLink((operation, forward) => {
+        return forward(operation).map(response => {
+            const context = operation.getContext();
+            const {
+                response: { headers }
+            } = context;
+
+            if (headers) {
+                console.log(headers.get('token-expired'));
+            }
+
+            return response;
+        })
+    });
+
+    const logoutLink = onError(({ graphQLErrors, networkError, operation, forward }) => {
+        const context = operation.getContext();
+
+        if (graphQLErrors) {
+            for (const error of graphQLErrors) {
+                if (error.extensions.code === 'UNAUTHENTICATED') {
+                    const {
+                        response: { headers }
+                    } = context;
+
+                    if (headers && headers.has('token-expired')) {
+                        console.log('refreshToken');
+                    } else {
+                        console.log('log the dude out');
+                    }
+                }
+            }
         }
     });
-    
+
     return {
-        link: logoutLink.concat(http),
+        link: afterwareLink.concat(logoutLink).concat(http),
         cache: new InMemoryCache(),
     };
 }
